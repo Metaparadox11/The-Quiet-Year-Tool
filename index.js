091f926392ec882e69377e38733baec9f3889bcd
Olivia Montoya <metaparadox11@gmail.com>
2020-03-24 03:54:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/static'));// Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, '/static/index.html'));
+  //response.sendFile(path.join(__dirname, '/static/index.html'));
+  response.sendFile(path.join(__dirname, 'index.html'));
 });// Starts the server.
 
 app.listen(PORT, () => {
