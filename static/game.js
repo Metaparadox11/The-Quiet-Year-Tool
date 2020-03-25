23a31eecb109a54e3fb5ebb6e7bcecb09e08c391
Olivia Montoya <metaparadox11@gmail.com>
2020-03-25 17:29:50 -0400

    Update game.js

--- static/game.js
+++ static/game.js
@@ -63,10 +63,10 @@ const API_URL = 'https://deckofcardsapi.com/api/deck/';
 
 const NEW_SHUFFLED_DECK = 'new/shuffle/';
 
-const HEARTS_CARDS = '?cards=AH,2H,3H,4H,5H,6H,7H,8H,9H,10H,JH,QH,KH';
-const SPADES_CARDS = '?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,10S,JS,QS,KS';
-const CLUBS_CARDS = '?cards=AC,2C,3C,4C,5C,6C,7C,8C,9C,10C,JC,QC,KC';
-const DIAMONDS_CARDS = '?cards=AD,2D,3D,4D,5D,6D,7D,8D,9D,10D,JD,QD,KD';
+const HEARTS_CARDS = '?cards=AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH';
+const SPADES_CARDS = '?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS';
+const CLUBS_CARDS = '?cards=AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC';
+const DIAMONDS_CARDS = '?cards=AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD';
 
 const ENTIRE_API_URL_HEARTS = API_URL + NEW_SHUFFLED_DECK + HEARTS_CARDS;
 const ENTIRE_API_URL_SPADES = API_URL + NEW_SHUFFLED_DECK + SPADES_CARDS;
@@ -129,6 +129,8 @@ body.appendChild(button);
 
 const DRAW_ONE = '/draw/?count=1';
 
+var frostShepherds = false;
+var fsCode = 'KS';
 
 button.addEventListener ("click", function() {
     const ENTIRE_API_URL_DRAW_HEARTS = API_URL + ids.hearts + DRAW_ONE;
@@ -150,8 +152,8 @@ button.addEventListener ("click", function() {
             })
             .catch(error => console.log('Error', error));
     } else {
-        if (spadesRemaining > 0) {
-            axios.get(ENTIRE_API_URL_DRAW_SPADES)
+        if (diamondsRemaining > 0) {
+            axios.get(ENTIRE_API_URL_DRAW_DIAMONDS)
                 .then(response => {
                     if (response.data.success) {
                         var imageURL = response.data.cards[0].image;
@@ -160,7 +162,7 @@ button.addEventListener ("click", function() {
                         img.width = 30;
                         body.appendChild(img);
                     }
-                    spadesRemaining = response.data.remaining;
+                    diamondsRemaining = response.data.remaining;
                 })
                 .catch(error => console.log('Error', error));
         } else {
@@ -178,19 +180,24 @@ button.addEventListener ("click", function() {
                     })
                     .catch(error => console.log('Error', error));
             } else {
-                if (diamondsRemaining > 0) {
-                    axios.get(ENTIRE_API_URL_DRAW_DIAMONDS)
-                        .then(response => {
-                            if (response.data.success) {
-                                var imageURL = response.data.cards[0].image;
-                                var img = document.createElement('img');
-                                img.src = imageURL;
-                                img.width = 30;
-                                body.appendChild(img);
-                            }
-                            diamondsRemaining = response.data.remaining;
-                        })
-                        .catch(error => console.log('Error', error));
+                if (frostShepherds == false) {
+                    if (spadesRemaining > 0) {
+                        axios.get(ENTIRE_API_URL_DRAW_SPADES)
+                            .then(response => {
+                                if (response.data.success) {
+                                    var imageURL = response.data.cards[0].image;
+                                    var img = document.createElement('img');
+                                    img.src = imageURL;
+                                    img.width = 30;
+                                    body.appendChild(img);
+                                    if (response.data.cards[0].code == fsCode) {
+                                        frostShepherds = true;
+                                    }
+                                }
+                                spadesRemaining = response.data.remaining;
+                            })
+                            .catch(error => console.log('Error', error));
+                    }
                 }
             }
         }
